98ebe5754df66b4f00d27a73d77d5ec1f63eb82d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-27 14:50:17 +0200

    scaled_dot_product is completed

--- src/scaled_dot_product.py
+++ src/scaled_dot_product.py
@@ -12,25 +12,40 @@ from mask import padding_mask
 def scaled_dot_product(
     query: torch.Tensor, key: torch.Tensor, values: torch.Tensor, mask=None
 ):
-    result = torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(
-        (query.size(1) * query.size(3))
-    )
+    if (
+        isinstance(query, torch.Tensor)
+        and isinstance(key, torch.Tensor)
+        and isinstance(values, torch.Tensor)
+    ) and (query.size() == key.size() == values.size()):
 
-    if mask is not None:
-        result += padding_mask(mask=mask)
+        result = torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(
+            (query.size(1) * query.size(3))
+        )
 
-    result = torch.softmax(input=result, dim=-1)
+        if mask is not None:
+            result += padding_mask(mask=mask)
 
-    result = torch.matmul(result, values)
+        result = torch.softmax(input=result, dim=-1)
 
-    return result
+        result = torch.matmul(result, values)
+
+        return result
+    else:
+        raise TypeError(
+            "The query, key, and values must be of type torch.Tensor and same shape".capitalize()
+        )
 
 
 if __name__ == "__main__":
-    scaled = scaled_dot_product(
-        torch.randn(40, 8, 200, 64),
-        torch.randn(40, 8, 200, 64),
-        torch.randn(40, 8, 200, 64),
+    parser = argparse.ArgumentParser(
+        description="Scaled dot product for Transformer".title()
     )
+    parser.parse_args()
 
-    print(scaled.size())
+    query = torch.randn((40, 8, 200, 64))
+    key = torch.randn((40, 8, 200, 64))
+    values = torch.randn((40, 8, 200, 64))
+
+    attention_output = scaled_dot_product(query=query, key=key, values=values)
+
+    assert attention_output.size() == (40, 8, 200, 64)
