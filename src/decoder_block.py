291075903db4365b92797d25c1dc82781ed1e089
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-27 17:41:20 +0200

    Decoder is done

--- src/decoder_block.py
+++ src/decoder_block.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import torch
 import argparse
 import torch.nn as nn
+from torchview import draw_graph
 
 sys.path.append("/src/")
 
+from utils import config
 from layer_normalization import LayerNormalization
 from feedforward_network import PointWiseFeedForward
 from multihead_attention import MultiHeadAttentionLayer
@@ -19,6 +22,7 @@ class DecoderBlock(nn.Module):
         feedforward: int = 2048,
         dropout: float = 0.1,
         epsilon: float = 1e-6,
+        display: bool = False,
     ):
         super(DecoderBlock, self).__init__()
 
@@ -27,6 +31,7 @@ class DecoderBlock(nn.Module):
         self.feedforward = feedforward
         self.dropout = dropout
         self.epsilon = epsilon
+        self.display = display
 
         self.masked_multihead_attention = MultiHeadAttentionLayer(
             dimension=self.dimension,
@@ -79,5 +84,75 @@ class DecoderBlock(nn.Module):
 
 
 if __name__ == "__main__":
-    decoder = DecoderBlock(dimension=512, heads=8, dropout=0.1, epsilon=1e-6)
-    print(decoder(torch.randn((40, 200, 512)), torch.randn((40, 200, 512))).size())
+    parser = argparse.ArgumentParser(
+        description="Decoder block for the Transformer model".title()
+    )
+    parser.add_argument(
+        "--dimension",
+        type=int,
+        default=512,
+        help="Dimension of the input tensor".capitalize(),
+    )
+    parser.add_argument(
+        "--heads",
+        type=int,
+        default=8,
+        help="Number of attention heads".capitalize(),
+    )
+    parser.add_argument(
+        "--dropout",
+        type=float,
+        default=0.1,
+        help="Dropout rate".capitalize(),
+    )
+    parser.add_argument(
+        "--epsilon",
+        type=float,
+        default=1e-6,
+        help="Epsilon for the layer normalization".capitalize(),
+    )
+    parser.add_argument(
+        "--feedforward",
+        type=int,
+        default=2048,
+        help="Feedforward dimension".capitalize(),
+    )
+    parser.add_argument(
+        "--display",
+        type=bool,
+        default=False,
+        help="Display the model architecture".capitalize(),
+    )
+
+    args = parser.parse_args()
+
+    dimension = args.dimension
+    heads = args.heads
+    dropout = args.dropout
+    epsilon = args.epsilon
+    feedforward = args.feedforward
+
+    decoder = DecoderBlock(
+        dimension=dimension,
+        heads=heads,
+        dropout=dropout,
+        epsilon=epsilon,
+        feedforward=feedforward,
+    )
+    X = torch.randn((40, 200, dimension))
+    y = torch.randn((40, 200, dimension))
+
+    assert decoder(X, y).size() == (
+        40,
+        200,
+        dimension,
+    ), "Model output size is incorrect from decoder block".capitalize()
+
+    if args.display:
+        path = config()["path"]["FILES_PATH"]
+
+        draw_graph(model=decoder, input_data=(X, y)).visual_graph.render(
+            filename=os.path.join(path, "one_decoder"), format="png"
+        )
+
+        print(f"Model architecture is saved in the path {path}".capitalize())
