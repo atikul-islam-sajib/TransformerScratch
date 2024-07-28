8fd09e99c18116bf209ebc89e2e08a6a034be7d0
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-28 21:35:08 +0200

    unittest prototype is added

---
+++ unittest/test.py
@@ -0,0 +1,18 @@
+import sys
+import torch
+import torch.nn as nn
+import unittest
+
+sys.path.append("./src/")
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
